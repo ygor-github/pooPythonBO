91b0343e5e1a38488b18759a6867ded3ebc417a7
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-11 20:10:40 -0300

    Creating Object, Ref. Class, Instance and testing

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
-print('Testing Dev')
+class Main:
+    pass
+
+print('Testing Project')
+
+from Cliente import Cliente
+
+c1 = Cliente('Joao', '114444-2222')
+
+print(c1)
+print(c1.nome,' e ',c1.telefone)
