06e817a3eef505a96bd9a2d7cfa414e6fa9ba873
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-11 20:24:46 -0300

    Project Bank Controle

---
+++ Conta.py
@@ -0,0 +1,5 @@
+class Conta:
+    def __init__(self,titular, numero, saldo):
+        self.titular=titular
+        self.numero=numero
+        self.saldo=0
--- Main.py
+++ Main.py
@@ -5,7 +5,12 @@ print('Testing Project')
 
 from Cliente import Cliente
 
+from Conta import Conta
+
 c1 = Cliente('Joao', '114444-2222')
+conta = Conta(c1.nome,6565,0)
 
 print(c1)
 print(c1.nome,' e ',c1.telefone)
+
+print(conta.titular,' Número: ',conta.numero,' Seu saldo: ',conta.saldo)
