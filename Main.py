7559375582fc0e1b87ebd0c91f3c83f8314a1798
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-12 10:37:16 -0300

    Testing methods"

--- Main.py
+++ Main.py
@@ -10,7 +10,12 @@ from Conta import Conta
 c1 = Cliente('Joao', '114444-2222')
 conta = Conta(c1.get_nome(),6565,200)
 
-print(c1)
-print(c1.get_nome(),' e ',c1._telefone)
+#print(c1)
+#print(c1.get_nome(),' e ',c1._telefone)
 
-print(conta._titular,' Número: ',conta._numero,' Seu saldo: ',conta._saldo)
+#print(conta._titular,' Número: ',conta._numero,' Seu saldo: ',conta._saldo)
+
+conta.desposita(100)
+conta.extrato()
+conta.saque(150)
+conta.extrato()
