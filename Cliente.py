1f0e8590879e4702e236e65908ceb70ebe10deda
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-12 10:10:51 -0300

    Correction idexing method in Cliente Class

--- Cliente.py
+++ Cliente.py
@@ -1,12 +1,13 @@
 class Cliente:
     def __init__(self, n, fone):
+
         self._nome = n
         self._telefone = fone
 
-#Method get
-def get_nome(self):
-    return self._nome
+    #Method get
+    def get_nome(self):
+        return self._nome
 
-#Method set
-def set_nome(self, nome):
-    self._nome = nome
+    #Method set
+    def set_nome(self, nome):
+        self._nome = nome
--- Main.py
+++ Main.py
@@ -8,9 +8,9 @@ from Cliente import Cliente
 from Conta import Conta
 
 c1 = Cliente('Joao', '114444-2222')
-conta = Conta(c1._nome,6565,0)
+conta = Conta(c1.get_nome(),6565,200)
 
 print(c1)
-print(c1._nome,' e ',c1._telefone)
+print(c1.get_nome(),' e ',c1._telefone)
 
 print(conta._titular,' Número: ',conta._numero,' Seu saldo: ',conta._saldo)
